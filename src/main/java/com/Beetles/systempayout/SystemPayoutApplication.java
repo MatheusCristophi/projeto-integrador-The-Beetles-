16a0b201289afd4b9a7135099acb5453d2b1412f
Anderson Junior <anderson.s.junior04@gmail.com>
2026-06-03 14:02:52 -0300

    alteração background-color & refatoração de imagens

--- src/main/java/com/Beetles/systempayout/SystemPayoutApplication.java
+++ src/main/java/com/Beetles/systempayout/SystemPayoutApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SystemPayoutApplication {
+public class  SystemPayoutApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemPayoutApplication.class, args);
     }
