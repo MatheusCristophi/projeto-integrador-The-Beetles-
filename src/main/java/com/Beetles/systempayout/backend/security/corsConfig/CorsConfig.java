e9e02e67490bc57301ad41d95a018972f097ecc7
Matheus Cristophi <matheuscr909@gmail.com>
2026-07-23 21:16:41 -0300

    Realizando o Deploy da Aplicação

--- src/main/java/com/Beetles/systempayout/backend/security/corsConfig/CorsConfig.java
+++ src/main/java/com/Beetles/systempayout/backend/security/corsConfig/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://gerenciamento-academias.up.railway.app/")
+                .allowedOrigins("https://gerenciamento-academias.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
